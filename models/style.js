4f6af457cb27165d90bc2ff623ca4daba27d56bd
mike881205 <mike881205@gmail.com>
2021-11-11 14:00:28 -0500

    updated models

--- models/style.js
+++ models/style.js
@@ -8,9 +8,41 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.TEXT,            
             allowNull: false
         },
-        keyWords: {
-            type: DataTypes.TEXT,            
+        gender_age: {
+            type: DataTypes.STRING,            
             allowNull: false
+        },
+        fabric: {
+            type: DataTypes.STRING,            
+            allowNull: false
+        },
+        sleeveLength: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        sleeveType: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        neckline: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        fleeceType: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        hatType: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        hatClosure: {
+            type: DataTypes.STRING,            
+            allowNull: true
+        },
+        hatStructure: {
+            type: DataTypes.STRING,            
+            allowNull: true
         },
         description: {
             type: DataTypes.TEXT,            
--- seeds/styles.sql
+++ seeds/styles.sql
@@ -1,59 +1,61 @@
 USE garmentWholesale;
 
-INSERT INTO Styles (styleNum, styleName, keyWords, description, BrandId)
+INSERT INTO Styles 
+(styleNum, styleName, gender_age, fabric, sleeveLength, sleeveType, neckline, fleeceType, hatType, hatClosure, hatStructure, 
+description, BrandId)
 VALUES
 -- 1-Alpha Apparel
 -- Style 1
-("1003", "Cotton Crew Neck T-Shirt", "Short Sleeve, Tee Shirt, 100% Cotton, Crew Neck",
+("1003", "Cotton Crew Neck T-Shirt", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL,
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 1),
 -- Style 2
-("1103CVC", "Heather Crew Neck T-Shirt", "Short Sleeve, Tee Shirt, Cotton/Poly, Crew Neck", 
+("1103CVC", "Heather Crew Neck T-Shirt", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 1),
 -- Style 3
-("1053", "Cotton Crew Neck Long Sleeve Tee", "Long Sleeve, Tee Shirt, 100% Cotton, Crew Neck", 
+("1053", "Cotton Crew Neck Long Sleeve Tee", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", 1),
 -- Style 4
-("8043", "Cotton Tank Top", "Tank Top, 100% Cotton", 
+("8043", "Cotton Tank Top", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1),
 
 -- 2-New Level
 -- Style 5
-("2300", "Cotton V-Neck T-Shirt", "Short Sleeve, Tee Shirt, 100% Cotton, V-Neck", 
+("2300", "Cotton V-Neck T-Shirt", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 2),
 -- Style 6
-("1060", "Triblend Crew Neck T-Shirt", "Short Sleeve, Tee Shirt, Cotton/Poly/Rayon, Crew Neck", 
+("1060", "Triblend Crew Neck T-Shirt", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 2),
 -- Style 7
-("6340", "Cotton Crew Neck Ringer Tee", "Short Sleeve, Tee Shirt, 100% Cotton, Crew Neck, Ringer", 
+("6340", "Cotton Crew Neck Ringer Tee", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", 2),
 -- Style 8
-("6305", "Cotton Long Sleeve Raglan Tee", "Long Sleeve, Tee Shirt, 100% Cotton, Crew Neck, Raglan", 
+("6305", "Cotton Long Sleeve Raglan Tee", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", NULL, NULL, NULL, NULL, 
 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 2),
 
 -- 3-Int'l Traders Outerwear
 -- Style 9
-("GG5400", "Crew Neck Sweater", "Outerwear, Sweater, 100% Cotton, Crew Neck", 
+("GG5400", "Crew Neck Sweater", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", "fleeceType", NULL, NULL, NULL, 
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 3),
 -- Style 10
-("XFA2000", "Pullover Hoodie", "Outerwear, Hooded, Cotton/Poly, Pullover", 
+("XFA2000", "Pullover Hoodie", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", "fleeceType", NULL, NULL, NULL, 
 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 3),
 -- Style 11
-("XFA2000Z", "Zip Up Hoodie", "Outerwear, Hooded, Cotton/Poly, Zip", 
+("XFA2000Z", "Zip Up Hoodie", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", "fleeceType", NULL, NULL, NULL, 
 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", 3),
 -- Style 12
-("MRP50PNT", "Cotton Joggers", "Outerwear, Pants, Jogger, 100% Cotton", 
+("MRP50PNT", "Cotton Joggers", "gender_age", "fabric", "sleeveLength", "sleeveType", "neckline", "fleeceType", NULL, NULL, NULL, 
 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3),
 
 -- 4-Cloud-Nine Headwear
 -- Style 13
-("6060", "Flat Bill Trucker Hat", "Headwear, Snapback, Structured, Five-panel, High-profile, Cotton/Poly Mesh", 
+("6060", "Flat Bill Trucker Hat", "gender_age", "fabric", NULL, NULL, NULL, NULL, "hatType", "hatClosure", "hatStructure", 
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 4),
 -- Style 14
-("8906", "Flat Bill Snapback Hat", "Headwear, Snapback, Structured, Five-panel, High-profile, Cotton/Poly", 
+("8906", "Flat Bill Snapback Hat", "gender_age", "fabric", NULL, NULL, NULL, NULL, "hatType", "hatClosure", "hatStructure", 
 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 4),
 -- Style 15
-("5100", "Beanie", "Headwear, 100% Acrylic", 
+("5100", "Beanie", "gender_age", "fabric", NULL, NULL, NULL, NULL, "hatType", "hatClosure", "hatStructure", 
 "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", 4),
 -- Style 16
-("4562", "Dad Hat", "Headwear, Adjustable, Unstructured, Six-Panel, Low-profile, 100% Cotton", 
+("4562", "Dad Hat", "gender_age", "fabric", NULL, NULL, NULL, NULL, "hatType", "hatClosure", "hatStructure", 
 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 4);
